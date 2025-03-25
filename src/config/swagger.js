f92b3161803ca6d940dfc432fedf19b305937f66
khalil kraiem <khalilmedkraiem@gmail>
2025-03-25 22:03:37 +0100

    swagger

---
+++ src/config/swagger.js
@@ -0,0 +1,20 @@
+// const swaggerAutogen = require("swagger-autogen")();
+import swaggerAutogen from "swagger-autogen";
+
+const doc = {
+  info: {
+    title: "API Documentation",
+    description: "توثيق API بطريقة تلقائية بدون YAML أو تعليقات كثيرة",
+    version: "1.0.0",
+  },
+  host: "localhost:3000",
+  schemes: ["http"],
+  tags: [
+    { name: "Users", description: "إدارة المستخدمين" }
+  ]
+};
+
+const outputFile = "./swagger-output.json";
+const routes = ["../app.js"]; 
+
+swaggerAutogen(outputFile, routes, doc);
